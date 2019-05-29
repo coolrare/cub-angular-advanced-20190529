5b8b51f23692c4eb33c7b6304e6d615a02de6499
Will <doggy.huang@gmail.com>
2019-05-29 18:49:12 +0800

    建立 buttons 與 cards 元件 (並註冊到 components 模組)

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ComponentsRoutingModule } from './components-routing.module';
+import { CardsComponent } from './cards/cards.component';
+import { ButtonsComponent } from './buttons/buttons.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [CardsComponent, ButtonsComponent],
   imports: [
     CommonModule,
     ComponentsRoutingModule

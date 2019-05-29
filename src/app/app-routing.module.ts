b18b8d510a7b17146efcb34bd1a8246e257055f7
Will <doggy.huang@gmail.com>
2019-05-29 14:53:21 +0800

    加入 Dashboard 元件並設定 <router-outlet> Directive 到正確位置

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Page1Component } from './page1/page1.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
+  { path: '', component: DashboardComponent },
   { path: 'page1', component: Page1Component }
 ];
 
